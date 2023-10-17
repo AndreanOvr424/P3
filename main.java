d50a4cacb191d05fb5ac8e22671e1549066cf28e
AndreanOvr424 <148152015+AndreanOvr424@users.noreply.github.com>
2023-10-17 19:54:57 +0300

    Create main.java
    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package P3;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("\tВаш заказ принят.");
+        System.out.println("Товар Яблоки в количестве 10 кг");
+        System.out.println("Прибудет к вам в ближайшее время.");
+        System.out.println("Товар доставим по адресу: СПБ");
+        System.out.println("Ваш контактный номер: 9046164231.");
+    }
+
+}
